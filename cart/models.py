c786a5a73bdc35a1c37966c185100e5ca762feaa
Ross Klaiber <rossk31@icloud.com>
2025-09-18 15:49:32 -0400

    Work from Cursor

--- cart/models.py
+++ cart/models.py
@@ -3,6 +3,31 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 from movies.models import Movie
+
+class Cart(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, default='Cart')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.name} - {self.user.username}"
+    
+    class Meta:
+        unique_together = ['name', 'user']  # Each user can have unique cart names
+
+class CartItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    
+    def __str__(self):
+        return f"{self.movie.name} x{self.quantity} in {self.cart.name}"
+    
+    class Meta:
+        unique_together = ['cart', 'movie']  # Each movie can only appear once per cart
+
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
     total = models.IntegerField()
